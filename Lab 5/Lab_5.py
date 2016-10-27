8d52b3fba03c3399aef05bf22e1a065bbf48087e
HashiR Salam <HashiR Salam>
2016-10-27 13:00:46 +0500

    Finalized the program

--- Lab 5/Lab_5.py
+++ Lab 5/Lab_5.py
@@ -2,79 +2,62 @@ import sys
 import os
 import string
 
-
-def index_text_file(txt_filename, idx_filename = "index.txt",delimiter_chars=",.;:!?"):
+def index_text_file(txt_filename, resultant_file = "index.txt",delimiter_chars=",.;:!?"):
     
-    txt_fil = open(txt_filename, "r")
-    word_occurrences = {}
-    line_num = 0
+    text_file = open(txt_filename, "r")                             #Open file in read mode
+    dict_of_words = {}                                              #Dictionary to store words           
+    line = 0                                                        #Store Line number    
 
-    for lin in txt_fil:
-        line_num += 1
-        print("line_num", line_num)
-        # Split the line into words delimited by whitespace.
-        words = lin.split()
-        print("words", words)
-        # Remove unwanted delimiter characters adjoining words.
-        words2 = [ word.strip(delimiter_chars) for word in words ]
-        print("words2", words2)
-        # Find and save the occurrences of each word in the line.
-        for word in words2:
-            if word_occurrences.has_key(word):
-                word_occurrences[word].append(line_num)
+    for lin in text_file:
+        line += 1
+        words = lin.split()                                              # Split the line by whitespace.
+        clean_words = [ word.strip(delimiter_chars) for word in words ]  #Removing extra chracters 
+        for word in clean_words:
+            if dict_of_words.has_key(word):
+                dict_of_words[word].append(line)
             else:
-                word_occurrences[word] = [ line_num ]
+                dict_of_words[word] = [ line ]
+    word_keys = dict_of_words.keys()
+    length = len(word_keys)
+    length = str(length)
+    print ( length + " unique words found." + "   in "+ txt_filename)
+    word_keys = dict_of_words.keys()
+    word_keys.sort()                                                                 
 
-    print("Processed {} lines".format(line_num))
-
-    if line_num < 1:
-        print "No lines found in text file, no index file created."
-        txt_fil.close()
-        sys.exit(0)
-
-    # Display results.
-    word_keys = word_occurrences.keys()
-    print "{} unique words found.".format(len(word_keys))
-    #print("DEBUG: Word_occurrences", word_occurrences)
-    word_keys = word_occurrences.keys()
-    #print("DEBUG: word_keys", word_keys)
-
-    # Sort the words in the word_keys list.
-    word_keys.sort()
-    #print("DEBUG: after sort, word_keys", word_keys)
-
-    # Create the index file.
-    idx_fil = open(idx_filename, "a")
-
-    # Write the words and their line numbers to the index file.
-    # Since we read the text file sequentially, there is no need 
-    # to sort the line numbers associated with each word; they are 
-    # already in sorted order.
+    idx_fil = open(resultant_file, "a")                                  # Create the index file.
     for word in word_keys:
-        line_nums = word_occurrences[word]
+        line_nums = dict_of_words[word]
         idx_fil.write(word + " " +" Path : " + txt_filename + "( Line number: ")
-        for line_num in line_nums:
-            idx_fil.write("  " + str(line_num) + " ) ")
+        for line in line_nums:
+            idx_fil.write("  " + str(line) + " ) ")
         idx_fil.write("\n")
 
-    txt_fil.close()
+    text_file.close()                                                    #Closing both files
     idx_fil.close()
 
-
-
-
-
 #C:\\
-#for root, dirs, files in os.walk("../"):
-#    for file in files:
-#        if file.endswith(".txt"):
-#            file = os.path.join(root, file)
-#            #print(os.path.join(root, file))
-#            index_text_file(file)    
+#../
+print ("Welcome to my file indexer \n\n")
 
+while (True):
+    print ("(1) Create text file index \n\n")
+    print ("(2) Search keyword in index \n\n")
+    choice = raw_input("Enter choice number : ")
 
-with open("index.txt") as openfile:
-    for line in openfile:
-        for part in line.split():
-            if "Hashir" in part:
-                print part , line
+    if (choice == "1"):
+        print("Creating index file ... Please wait ..\n\n")  
+        for root, dirs, files in os.walk("C://Users//"):                #To search current Directory replace "C:\\" with "../"
+            for file in files:
+                if file.endswith(".txt"):
+                    file = os.path.join(root, file)
+                    index_text_file(file)
+        print("DONE\n\n")        
+
+    if (choice == "2"):
+        keyword = raw_input("Enter Keyword : ")
+        with open("index.txt") as openfile:
+            for line in openfile:
+                for part in line.split():
+                    if keyword in part:
+                        print ("\nFOUND: " + part + "\nPATH: "+ line)
+                   
